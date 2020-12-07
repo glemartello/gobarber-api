3fb6b2938cb226df396d36f11ee2d749c02d088e
Pedro Zocatelli <phz.pedro@gmail.com>
2020-12-07 18:17:45 -0300

    reset password test coverage

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,11 +1,20 @@
 import { container } from 'tsyringe';
 
-import IStorageProvider from './StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import '@modules/users/providers';
+import './providers';
 
-// import IMailProvider from './MailProvider/models/'
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
+container.registerSingleton<IAppointmentsRepository>(
+  'AppointmentsRepository',
+  AppointmentsRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
